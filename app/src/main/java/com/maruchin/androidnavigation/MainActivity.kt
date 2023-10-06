e89fc41c68208a83d07bed02cd6a21bd8d5367f3
Marcin Piekielny <marcinpk.mp@gmail.com>
2023-10-06 19:36:03 +0200

    Setup Hilt

--- app/src/main/java/com/maruchin/androidnavigation/MainActivity.kt
+++ app/src/main/java/com/maruchin/androidnavigation/MainActivity.kt
@@ -3,44 +3,14 @@ package com.maruchin.androidnavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.maruchin.androidnavigation.ui.theme.AndroidnavigationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidnavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MaterialTheme {
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidnavigationTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/maruchin/androidnavigation/SampleApplication.kt
@@ -0,0 +1,7 @@
+package com.maruchin.androidnavigation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SampleApplication : Application()
