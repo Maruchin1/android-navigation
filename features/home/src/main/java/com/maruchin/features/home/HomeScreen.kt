68f2c514579b7bd7e8a751b60b9c43c87f06ae3f
Marcin Piekielny <marcinpk.mp@gmail.com>
2023-10-07 21:21:01 +0200

    Update HomeScreen

--- features/home/src/main/java/com/maruchin/features/home/HomeScreen.kt
+++ features/home/src/main/java/com/maruchin/features/home/HomeScreen.kt
@@ -3,6 +3,7 @@
 package com.maruchin.features.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -13,15 +14,18 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -29,67 +33,149 @@ import coil.compose.AsyncImage
 import com.maruchin.data.categories.Category
 import com.maruchin.data.products.Product
 import com.maruchin.data.products.sampleProducts
+import java.net.URL
 
 @Composable
 fun HomeScreen() {
     val viewModel: HomeViewModel = hiltViewModel()
-    HomeScreen(products = viewModel.products)
+    HomeScreen(
+        products = viewModel.products,
+        onShowAllFromCategory = {}
+    )
 }
 
 @Composable
-internal fun HomeScreen(products: Map<Category, List<Product>>) {
+internal fun HomeScreen(
+    products: Map<Category, List<Product>>,
+    onShowAllFromCategory: (Category) -> Unit,
+) {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(text = "Home")
-                },
-                actions = {
-                    TextButton(onClick = { /*TODO*/ }) {
-                        Text(text = "Login")
-                    }
-                }
-            )
+            TopAppBar()
         },
         content = { padding ->
-            LazyColumn(
-                modifier = Modifier
-                    .padding(padding)
-                    .fillMaxSize()
-            ) {
-                items(products.entries.toList()) { (category, products) ->
-                    Column {
-                        Text(
-                            text = category.name,
-                            modifier = Modifier.padding(12.dp),
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                        LazyRow(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.spacedBy(12.dp),
-                            contentPadding = PaddingValues(12.dp)
-                        ) {
-                            items(products) { product ->
-                                ProductItem(product = product)
-                            }
-                        }
-                    }
-                }
+            CategoryProductsList(
+                products = products,
+                modifier = Modifier.padding(padding),
+                onShowAllFromCategory = onShowAllFromCategory,
+            )
+        }
+    )
+}
+
+@Composable
+private fun TopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "Home")
+        },
+        actions = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Login")
             }
         }
     )
 }
 
 @Composable
-private fun ProductItem(product: Product) {
-    Card(modifier = Modifier.width(100.dp)) {
-        AsyncImage(
-            model = product.image.toString(),
-            contentDescription = null,
+private fun CategoryProductsList(
+    products: Map<Category, List<Product>>,
+    onShowAllFromCategory: (Category) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(modifier),
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+    ) {
+        items(products.entries.toList()) { (category, products) ->
+            Column {
+                CategoryHeadline(name = category.name)
+                ProductRow(
+                    products = products,
+                    onShowAll = { onShowAllFromCategory(category) }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CategoryHeadline(name: String) {
+    Text(
+        text = name.replaceFirstChar { it.titlecase() },
+        modifier = Modifier.padding(12.dp),
+        style = MaterialTheme.typography.titleLarge
+    )
+}
+
+@Composable
+private fun ProductRow(products: List<Product>, onShowAll: () -> Unit) {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        contentPadding = PaddingValues(12.dp)
+    ) {
+        items(products) { product ->
+            ProductItem(
+                image = product.image,
+                title = product.title,
+                price = product.price
+            )
+        }
+        item {
+            AllProductsButton(onClick = onShowAll)
+        }
+    }
+}
+
+@Composable
+private fun ProductItem(image: URL, title: String, price: Float) {
+    Column(
+        modifier = Modifier.width(150.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        OutlinedCard {
+            AsyncImage(
+                model = image.toString(),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(3f / 4f)
+                    .padding(12.dp)
+            )
+        }
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodyMedium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = price.toString(),
+            style = MaterialTheme.typography.titleMedium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Composable
+private fun AllProductsButton(onClick: () -> Unit) {
+    OutlinedCard(modifier = Modifier.width(150.dp), onClick = onClick) {
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .aspectRatio(3f / 4f)
-        )
+                .aspectRatio(3f / 4f),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = "Show all",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.alpha(0.5f)
+            )
+        }
     }
 }
 
@@ -98,7 +184,8 @@ private fun ProductItem(product: Product) {
 private fun HomeScreenPreview() {
     MaterialTheme {
         HomeScreen(
-            products = sampleProducts.groupBy { it.category }
+            products = sampleProducts.groupBy { it.category },
+            onShowAllFromCategory = {}
         )
     }
 }
